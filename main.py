f3f74fd58c68329930e0c80c2ece35fb9971dbc5
tiqet <mail@example.com>
2024-08-20 08:47:29 +0200

    add game loop, black window, nice close

---
+++ main.py
@@ -0,0 +1,25 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from pygame import display
+
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
